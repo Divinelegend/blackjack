fa00ccad4948d15e69e70354be19fdd35f4a2667
Luong <devilluong@yahoo.com>
2018-02-10 12:15:29 -0800

    Fix the hand class to be compatable with the other class.
    Signed-off-by: Luong <devilluong@yahoo.com>

--- app/src/main/java/website/lnt/blackjack/Hand.java
+++ app/src/main/java/website/lnt/blackjack/Hand.java
@@ -1,8 +1,11 @@
 package website.lnt.blackjack;
 
 import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * Created by Luong on 2/3/2018.
@@ -10,32 +13,37 @@ import java.util.Arrays;
 
 public class Hand {
 
-    ArrayList<String> Cardinhand = new ArrayList<String>();
+    LinkedList<Card> Cardinhand = new LinkedList<>();
     private int mBet;
 
-    public void hit(String card) {
+    public void hit(Card card) {
         Cardinhand.add(card);
     }
 
-    public void doubledown() {
-        if (Player.checkWallet() > mBet) {
-            Player.setWallet(Player.checkWallet() - mBet);
+    public int doubledown(Player p) {
+        if (p.getWallet() > mBet) {
+            p.setWallet(p.getWallet() - mBet);
             mBet = mBet * 2;
-        } else
+        } else {
             Log.e("mydebug", "You do not have enough money!");
+            return -1;
+        }
+        return 0;
     }
 
     public int evalHand() {
         int pointHand = 0;
         int str = 0;
-        for (int i=0; i<Cardinhand.size(); i++) {
-            str = Card.getNum(Cardinhand.get(i));
+        Iterator iter = Cardinhand.iterator();
+        while (iter.hasNext()) {
+            Card c = (Card) iter.next();
+            str = c.getNum();
             if (str > 10)
                 str = 10;
-            if (Card.toString(Cardinhand.get(i)) == "AceofHeart" || "AceofDiamond" || "AceofSpace" || "AceofClub")
+            if (c.getNum() == 1)
                 str = 11;
             pointHand = pointHand + str;
-            if (pointHand > 21 && Card.toString(Cardinhand.get(i)) == "AceofHeart" || "AceofDiamond" || "AceofSpace" || "AceofClub")
+            if (pointHand > 21 && c.getNum() == 1)
                 pointHand = pointHand - 10;
         }
         return pointHand;
