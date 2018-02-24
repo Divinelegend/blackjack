26bb37472af696390507c73f03ec855e91385d8d
Thinh Nguyen <thinhjb@lnt.website>
2018-02-24 15:10:51 -0800

    Add init for MainActivity
    Nothing much here. Just some simple initialization for tests.
    
    Signed-off-by: Thinh Nguyen <thinhjb@lnt.website>

--- app/src/main/java/website/lnt/blackjack/MainActivity.java
+++ app/src/main/java/website/lnt/blackjack/MainActivity.java
@@ -2,12 +2,23 @@ package website.lnt.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private Blackjack game;
+    private Player currentPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentPlayer = new Player("Kaiju", false, 100);
+        game = new Blackjack(1, 1);
+        try {
+            game.addPlayer(currentPlayer);
+        } catch (BlackjackException e) {
+            Log.e("MainActivity", e.getMessage());
+        }
     }
 }
